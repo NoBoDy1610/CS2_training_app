a8ac737758e858c212c9430386f8e7f8283f7e5e
Nikodem <nikodem_czubak@wp.pl>
2024-12-09 21:16:50 +0100

    First big commit

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Definition of user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Model export
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+
+dotenv.config();
 
 const app = express();
+
+//Connection with mongoDB
+connectDB();
+
+// Middleware
 app.use(express.json());
-app.use(cors());
 
-// Połączenie z MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Połączono z MongoDB'))
-  .catch(err => console.error('Błąd połączenia:', err));
-
-// Przykładowa trasa
+// Routes
 app.get('/', (req, res) => {
-  res.send('Witaj w API!');
+	res.send('API is running...');
 });
 
-// Start serwera
+app.post('/api/users', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).json({ message: 'User created', user: newUser });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+app.put('/user/nikodem', (req, res) => {
+	res.sendStatus(200);
+});
+app.patch('/user/nikodem', (req, res) => {
+	res.sendStatus(200);
+});
+app.delete('/user/nikodem', (req, res) => {
+	res.sendStatus(200);
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
