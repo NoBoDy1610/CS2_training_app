5e31c852172b4ae41d3f55af1b6b1b74f22c2221
Nikodem <nikodem_czubak@wp.pl>
2025-03-17 16:36:31 +0100

    Main Section changes

--- frontend/src/components/MainSection.js
+++ frontend/src/components/MainSection.js
@@ -6,51 +6,31 @@ import '../styles/MainSection.css';
 
 const MainSection = () => {
 	const handleNavigation = (section) => {
-		switch (section) {
-			case 'reaction-time':
-				window.location.href = '/reaction-time';
-				break;
-			case 'aim-training':
-				window.location.href = '/aim-training';
-				break;
-			case 'map-guide':
-				window.location.href = '/map-guide';
-				break;
-			case 'tactics':
-				window.location.href = '/tactics';
-				break;
-			case 'player-settings':
-				window.location.href = '/player-settings';
-				break;
-			default:
-				alert('Sekcja nie istnieje!');
-		}
+		window.location.href = `/${section}`;
 	};
 
 	return (
 		<div className='main-section'>
-			<div className='tiles-container'>
+
+			<div className='tiles-grid'>
 				<div className='tile' onClick={() => handleNavigation('reaction-time')}>
-					<MdTimer size={40} />
+					<MdTimer size={50} />
 					<p>Ćwiczenie czasu reakcji</p>
 				</div>
 				<div className='tile' onClick={() => handleNavigation('aim-training')}>
-					<BiTargetLock size={40} />
+					<BiTargetLock size={50} />
 					<p>Ćwiczenie wspomagające celowanie</p>
 				</div>
 				<div className='tile' onClick={() => handleNavigation('map-guide')}>
-					<FaMapMarkedAlt size={40} />
+					<FaMapMarkedAlt size={50} />
 					<p>Przewodnik po mapach</p>
 				</div>
 				<div className='tile' onClick={() => handleNavigation('tactics')}>
-					<FaLightbulb size={40} />
+					<FaLightbulb size={50} />
 					<p>Nauka taktyk</p>
 				</div>
-				<div
-					className='tile'
-					onClick={() => handleNavigation('player-settings')}
-				>
-					<FaUsers size={40} />
+				<div className='tile' onClick={() => handleNavigation('player-settings')}>
+					<FaUsers size={50} />
 					<p>Ustawienia innych graczy</p>
 				</div>
 			</div>
